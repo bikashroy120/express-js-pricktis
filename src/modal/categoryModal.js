7481a4ac434454cd41de7f60d220631c384d918e
bikashroy120 <bikashroydt@gmail.com>
2023-07-28 23:18:50 +0600

    modal add

---
+++ src/modal/categoryModal.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+
+const categoryScema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Name is requerd"]
+    },
+    slug:{
+        type:String,
+        required:[true,"Slug is requerd"],
+        unique:true,
+        lowercase:true
+    }
+},{timestamps:true})
+
+
+const Category = mongoose.Model("Category",categoryScema)
+
+module.exports = Category
