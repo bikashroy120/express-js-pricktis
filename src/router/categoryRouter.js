57dd83a14b2bd75ef52ea44cd5696db696ea2809
bikashroy120 <bikashroydt@gmail.com>
2023-07-31 15:25:53 +0600

    add

--- src/router/categoryRouter.js
+++ src/router/categoryRouter.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post("/creact",isLoggedIn,isAdmin,creactCategory)
 router.get("/",getAllCategory)
 router.get("/:slug",getSingalCategory)
-router.get("/update/:slug",updateCategory)
+router.put("/update/:slug",updateCategory)
 
 
 module.exports = router
